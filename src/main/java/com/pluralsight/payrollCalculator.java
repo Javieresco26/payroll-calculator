d5f8d646eae4342c7e40d33c15c87deaf208e663
alexr <alexramirez92606@gmail.com>
2026-04-13 15:22:50 -0500

    payroll refactor

--- src/main/java/com/pluralsight/payrollCalculator.java
+++ src/main/java/com/pluralsight/payrollCalculator.java
@@ -5,19 +5,40 @@ import java.util.Scanner;
 public class payrollCalculator {
     public static void main(String[] args) {
 
+
         Scanner input = new Scanner(System.in);
 
+        String name = getEmployeeName(input);
+        double hoursWorked = getHoursWorked(input);
+        double payRate = getPayRate(input);
+
+        double grossPay = calculateGrossPay(hoursWorked, payRate);
+
+        displayPayroll(name, grossPay);
+
+        input.close();
+    }
+
+    public static String getEmployeeName(Scanner input) {
         System.out.println("Enter your name here: ");
-        String name = input.nextLine();
+        return input.nextLine();
+    }
 
+    public static double getHoursWorked(Scanner input) {
         System.out.println("Enter hours worked: ");
-        double hoursWorked = input.nextDouble();
+        return input.nextDouble();
+    }
 
+    public static double getPayRate(Scanner input) {
         System.out.println("Enter pay rate: ");
-        double payRate = input.nextDouble();
+        return input.nextDouble();
+    }
 
-        double grossPay = hoursWorked * payRate;
+    public static double calculateGrossPay(double hoursWorked, double payRate) {
+        return hoursWorked * payRate;
+    }
 
+    public static void displayPayroll(String name, double grossPay) {
         System.out.println("Employee Name: " + name);
         System.out.println("Gross Pay: $" + grossPay);
 
